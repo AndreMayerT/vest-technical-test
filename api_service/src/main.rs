d690ac219d12df03c898ddebb0158feb34f977bf
Andre Mayer <andremayerusa@gmail.com>
2023-09-28 23:04:23 -0300

    add routes for the service

--- api_service/src/main.rs
+++ api_service/src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+mod graphql;
+mod services;
+
+use async_graphql::{Schema,EmptySubscription, Request, http::{playground_source, GraphQLPlaygroundConfig}};
+use async_graphql_warp::GraphQLResponse;
+use crate::graphql::*;
+use std::convert::Infallible;
+use warp::{Filter, filters::BoxedFilter, Reply, http::Response};
+
+extern crate log;
+extern crate pretty_env_logger;
+
+fn make_routes() -> BoxedFilter<(impl Reply,)> {
+    let schema = Schema::build(Query, Mutation, EmptySubscription)
+    .finish();
+
+    let graphql_handler = warp::post().and(warp::path("graphql"))
+    .and(
+        async_graphql_warp::graphql(schema)
+        .and_then(|(schema, request): (Schema<_,_,_>, Request) | async move {
+            Ok::<_, Infallible>(GraphQLResponse::from(schema.execute(request).await))
+        })
+    );
+
+    let graphql_playground = warp::path("playground").map(|| {
+        Response::builder()
+        .header("content-type", "text/html")
+        .body(playground_source(GraphQLPlaygroundConfig::new("/graphql")))
+    });
+
+    graphql_handler
+    .or(graphql_playground)
+    .boxed()
+}
+
+#[tokio::main]
+async fn main() {
+
+    pretty_env_logger::init();
+
+    warp::serve(make_routes()).run(([127, 0, 0, 1], 8000)).await;
 }
